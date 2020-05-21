3eed87a6d51fb87c39b16891bd7365d1572946c7
jpwilhelms <github@familie-wilhelms.de>
2020-05-21 21:13:10 +0200

    update for new ss_oled version 4.1.3

--- kleinteillager-wagen/display.ino
+++ kleinteillager-wagen/display.ino
@@ -1,4 +1,4 @@
-#include <ss_oled.h>
+#include <ss_oled.h> // version 4.1.3
 
 // #define USE_BACKBUFFER
 
@@ -8,16 +8,33 @@ static uint8_t ucBackBuffer[1024];
 static uint8_t *ucBackBuffer = NULL;
 #endif
 
+#define MY_OLED OLED_128x64
+#define OLED_WIDTH 128
+#define OLED_HEIGHT 64
+#define OLED_ADDR -1
+#define RESET_PIN -1
+#define SCL_PIN 22
+#define INVERT 0
+#define FLIP180 0
+#define SDA_PIN 21
+#define USE_HW_I2C 1
+
+SSOLED ssoled;
+
 void initDisplay() {
-  if (oledInit(OLED_128x64, 0, 0, -1, -1, 400000L) != OLED_NOT_FOUND) { // @suppress("Invalid arguments")
-    oledSetBackBuffer(ucBackBuffer); // @suppress("Invalid arguments")
-    oledFill(0, 1);
-    oledWriteString(0, 0, 0, "press button to start", FONT_SMALL, 0, 1);
+  if (oledInit(&ssoled, MY_OLED, OLED_ADDR, FLIP180, INVERT, USE_HW_I2C, SDA_PIN, SCL_PIN, RESET_PIN, 400000L) != OLED_NOT_FOUND) {
+    Serial.println( "initialized oled display" );
+    oledSetBackBuffer(&ssoled, ucBackBuffer); // @suppress("Invalid arguments")
+    oledFill(&ssoled, 0, 1);
+    oledWriteString(&ssoled, 0, 0, 0, "press button to start", FONT_SMALL, 0, 1);
+  }
+  else {
+    Serial.println( "error initializing oled display" );
   }
 }
 
 void clearDisplay() {
-  oledFill(0, 1);
+  oledFill(&ssoled, 0, 1);
 }
 
 void toDisplay() {
@@ -32,5 +49,5 @@ void toDisplay() {
 void writeToDisplay( int x, int y, char* text, int value ) {
   char message[80];
   sprintf( message, "%s: %d   ", text, value );
-  oledWriteString(0, x, y, message, FONT_SMALL, 0, 1);
+  oledWriteString(&ssoled, 0, x, y, message, FONT_SMALL, 0, 1);
 }
