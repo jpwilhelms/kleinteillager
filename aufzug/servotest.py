3a10827c21f9f3bb37c1f92ac63435347b5092f5
Jan Wilhelms <github.com@familie-wilhelms.de>
2021-08-17 22:21:28 +0100

    updated

--- aufzug/servotest.py
+++ aufzug/servotest.py
@@ -22,6 +22,7 @@ pwm = Adafruit_PCA9685.PCA9685()
 # Configure min and max servo pulse lengths
 servo_min = 150  # Min pulse length out of 4096
 servo_max = 600  # Max pulse length out of 4096
+servo_nr = 0     # 0..16
  
 # Helper function to make setting a servo pulse width simpler.
 def set_servo_pulse(channel, pulse):
@@ -40,9 +41,9 @@ pwm.set_pwm_freq(60)
 print('Moving servo on channel 0, press Ctrl-C to quit...')
 while True:
     # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, servo_min)
+    pwm.set_pwm(servo_nr, 0, servo_min)
     time.sleep(1)
-    pwm.set_pwm(0, 0, servo_max)
+    pwm.set_pwm(servo_nr, 0, servo_max)
     time.sleep(1)
  
 
